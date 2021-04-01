3659f913a78b35e3674b344eb54cd3c08df57fbd
sailorj94 <jackniall9@gmail.com>
2021-04-01 16:59:20 +0100

    Move menu function to task_list module

--- start_code/data/task_list.py
+++ start_code/data/task_list.py
@@ -5,3 +5,14 @@ tasks = [
     { "description": "Feed Cat", "completed": False, "time_taken": 5 },
     { "description": "Walk Dog", "completed": True, "time_taken": 60 },
 ]
+
+def print_menu():
+    print("Options:")
+    print("1: Display All Tasks")
+    print("2: Get Uncompleted Tasks")
+    print("3: Get Completed Tasks")
+    print("4: Mark Task as Complete")
+    print("5: Get Tasks Which Take Longer Than a Given Time")
+    print("6: Find Task by Description")
+    print("7: Add a new Task to list")
+    print("Q or q: Quit")
